ebfae557f5c162a8d7b2e9053d7675ceaea13ea2
ttzztztz <ttzztztz@outlook.com>
2020-01-16 20:01:06 +0800

    add auto save code

--- src/app/detail/submit-code/submit-code.component.ts
+++ src/app/detail/submit-code/submit-code.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 import { LanguageService } from "src/app/service/language.service";
+import { Subject, interval } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   selector: "app-submit-code",
@@ -9,12 +11,29 @@ import { LanguageService } from "src/app/service/language.service";
 export class SubmitCodeComponent implements OnInit {
   language: string = "";
   code: string = "";
+  @Input() tid: string = "";
   @Input() submitting: boolean = false;
   @Output() submit = new EventEmitter<{ language: string; code: string }>();
+  private unsubscribe$: Subject<void>;
 
   constructor(public languageService: LanguageService) {}
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+  ngOnInit() {
+    this.unsubscribe$ = new Subject<void>();
 
-  ngOnInit() {}
+    if (localStorage.getItem(this.tid)) {
+      this.code = localStorage.getItem(this.tid);
+    }
+
+    interval(30 * 1000)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        localStorage.setItem(this.tid, this.code);
+      });
+  }
   handleSubmit = () => {
     this.submit.emit({ code: this.code, language: this.language });
   };
